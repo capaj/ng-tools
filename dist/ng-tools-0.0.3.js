3d3aee6c65b5fb231669d7ed34330fefc4aa4eb2
jis <jis@censhare.de>
2013-10-16 19:58:41 +0200

    added set and stored set

---
+++ dist/ng-tools-0.0.3.js
@@ -0,0 +1,298 @@
+angular.module('ng-tools', []);
+angular.module('ng-tools').factory('debounce',['$timeout', function ($timeout) {
+	/**
+	 * will cal fn once after timeout even if more than one call wdo debounced fn was made
+	 * @param {Function} fn to call debounced
+	 * @param {Number} timeout
+	 * @param {boolean} apply will be passed to $timeout as last param, if the debounce is triggering infinite digests, set this to false
+	 * @returns {Function} which you can call instead fn as if you were calling fn
+	 */
+	function debounce(fn, timeout, apply){
+		timeout = angular.isUndefined(timeout) ? 0 : timeout;
+		apply = angular.isUndefined(apply) ? true : apply; // !!default is true! most suitable to my experience
+		var nthCall = 0;
+		return function(){ // intercepting fn
+			var that = this;
+			var argz = arguments;
+			nthCall++;
+			var later = (function(version){
+				return function(){
+					if (version === nthCall){
+						return fn.apply(that, argz);
+					}
+				};
+			})(nthCall);
+			return $timeout(later, timeout, apply);
+		};
+	}
+	return debounce;
+}]);
+/**
+ *	directive include-in-scope
+ *	attribute include-in-scope: if you have a scope variable containing html, assign it to this attribute
+ *	attribute src: if you have a template in separate html file, which you want to load, assign the path to it to this attribute
+ *	onload attribute is evaluated when content is included, if present
+ */
+angular.module('ng-tools').directive('includeInScope',
+	['$http', '$templateCache', '$anchorScroll', '$compile', '$animate',
+		function($http,   $templateCache,   $anchorScroll,   $compile, $animate) {
+			return {
+				restrict: 'ECA',
+				terminal: true,
+				compile: function(element, attr) {
+					var onloadExp = attr.onload || '';
+					var autoScrollExp = attr.autoscroll;
+
+					return function(scope, element) {
+						var changeCounter = 0;
+						var includeCompile = function (html) {
+							element.html(html);
+							$compile(element.contents())(scope);
+
+							if (angular.isDefined(autoScrollExp) && (!autoScrollExp || scope.$eval(autoScrollExp))) {
+								$anchorScroll();
+							}
+
+							scope.$emit('$includeContentLoaded');
+							scope.$eval(onloadExp);
+						};
+
+						var clearContent = function() {
+							$animate.leave(element);
+							element.html('');
+						};
+
+						if (attr.includeInScope) {
+							var html = scope.$eval(attr.includeInScope);
+							includeCompile(html);
+						}else{
+							scope.$watch(function () {
+								return attr.src;
+							}, function ngIncludeWatchAction(src) {
+								var thisChangeId = ++changeCounter;
+
+								if (src) {
+									$http.get(src, {cache: $templateCache}).success(function(response) {
+										if (thisChangeId !== changeCounter) return;
+										includeCompile(response);
+
+									}).error(function() {
+										if (thisChangeId === changeCounter) clearContent();
+									});
+								} else clearContent();
+							});
+						}
+					};
+				}
+			};
+		}
+	]
+);
+angular.module('ng-tools').factory('Set', function () {
+    /**
+     * return new instance of a Set
+     * @param {Function} [hashFunction] defaults to JSON.stringify
+     * @constructor
+     */
+    function Set(hashFunction) {
+        this.hashFn = hashFunction || JSON.stringify;
+        this.values = {};
+        this.size = 0;
+    }
+
+    Set.prototype = {
+        /**
+         * @param value
+         * @returns {boolean} true when item was added
+         */
+        add: function add(value) {
+            var r = !this.contains(value);
+            if (r) {
+                //does not contain
+                this.values[this.hashFn(value)] = value;
+                this.size++;
+            }
+            return r;
+        },
+        /**
+         * @param value
+         * @returns {boolean} true when item was replaced, false when just added
+         */
+        addReplace: function addReplace(value) {
+            var r = this.remove(value);
+            this.add(value);
+            return r;
+        },
+        /**
+         * @param {*} obj
+         * @returns {*} stored value or null if the hash did not found anything
+         */
+        getValue: function getValue(obj) {
+            var hash = this.hashFn(obj);
+            if (this.values.hasOwnProperty(hash)) {
+                return this.values[hash];
+            }
+            return null;
+        },
+        /**
+         * @param value
+         * @returns {boolean} whether it removed the item
+         */
+        remove: function remove(value) {
+            var r = this.contains(value);
+            if (r) {
+                // does copntain
+                delete this.values[this.hashFn(value)];
+                this.size--;
+            }
+            return r;
+        },
+        /**
+         * @param value
+         * @returns {boolean}
+         */
+        contains: function contains(value) {
+            return typeof this.values[this.hashFn(value)] !== "undefined";
+        },
+        /**
+         *
+         * @returns {number}
+         */
+        size: function size() {
+            return this.size;
+        },
+        /**
+         * runs iteratorFunction for each value of set
+         * @param {Function} iteratorFunction
+         * @param {*} [thisObj] this context for iterator
+         */
+        each: function each(iteratorFunction, thisObj) {
+            for (var value in this.values) {
+                var contx = thisObj || this.values[value];
+                iteratorFunction.call(contx, this.values[value]);
+            }
+        },
+        /**
+         *
+         * @returns {Array}
+         */
+        toArray: function () {
+            var r = [];
+            for(var i in this.values){
+                r.push(this.values[i]);
+            }
+            return r;
+        }
+    };
+    return Set;
+});
+
+/**
+ * set which is automatically stored in local storage, offers events to hook up syncing to the server, depends on
+ * storage injectable. storage injectable must have "get(key)" and "set(key, value)" method
+ */
+angular.module('ng-tools').factory('StoredSet',
+    /**
+     *
+     * @param Set
+     * @param {Object} storage
+     * @param {Function} storage.get
+     * @param {Function} storage.set
+     * @param $log
+     * @returns {Function}
+     */
+    function (Set, storage, $log) {
+	/**
+	 * @param {Object} opts typical call option object would look like:  {hashFn: .., storageKey: 'LSkey'}
+     * @param {Function} opts.hashFn function for hashing
+     * @param {string} opts.storageKey key for storing the set
+	 * @constructor
+	 */
+	function StoredSet(opts) {
+        var self = this;
+        if (!opts.storageKey) {
+            $log.error("storageKey property must be provided for storedSet");
+            return;
+        }
+        angular.extend(self, opts);
+
+        /**
+         * @type {Set}
+         */
+        this.set = new Set(self.hashFn);
+
+        this.onInit(storage.get(self.storageKey));
+    }
+	/**
+	 * Prototype
+	 * @type {{add: Function, addReplace: Function, remove: Function, toArray: Function, onAdd: Function, onRemove: Function, onInit: Function}}
+	 */
+    StoredSet.prototype = {
+		/**
+		 * @param {*} item
+		 * @param {boolean} [skipSave]
+		 * @returns {boolean} true when item added, false when not added
+		 */
+		add: function (item, skipSave) {
+			var added = this.set.add(item);
+			this.onAdd(item, added);
+			added && this.save(skipSave);
+			return added;
+		},
+		/**
+		 * @param {*} item
+		 * @param {boolean} [skipSave] if true, then the save to storage will be skipped
+		 */
+        addReplace: function (item, skipSave) {
+			var replaced = this.set.addReplace(item);
+            this.onAdd(item, replaced);
+            this.save(skipSave);
+			return !replaced;
+        },
+	    /**
+	     * Remove from storage
+	     * @param item
+	     * @returns {boolean}
+	     */
+        remove: function (item) {
+			var removed = this.set.remove(item);
+			if (removed) {
+                this.save();
+                this.onRemove(item);
+            }
+			return removed;
+        },
+	    /**
+	     * each similar to forEach with arrays
+	     */
+	    each: function(){
+		    return this.set.each.apply(this.set, arguments);
+	    },
+	    /**
+	     * To array
+	     * @returns {Array}
+	     */
+        toArray: function () {
+            return this.set.toArray();
+        },
+		/**
+		 * @param skip the save
+		 */
+		save: function (skip) {
+			!skip && storage.set(this.storageKey, angular.copy(this.toArray())); // angular.copy removes $$hashKey
+		},
+        //events
+        onAdd: function (item, replaced) {},
+        onRemove: function (item) {},
+        onInit: function (cached) {
+            if (cached && Array.isArray(cached)) {
+                cached.forEach(angular.bind(this, function (obj) {
+					this.add(obj, true);
+                }) );
+            }
+
+        }
+    };
+    return StoredSet;
+});
+
---
+++ src/set.js
@@ -0,0 +1,98 @@
+angular.module('ng-tools').factory('Set', function () {
+    /**
+     * return new instance of a Set
+     * @param {Function} [hashFunction] defaults to JSON.stringify
+     * @constructor
+     */
+    function Set(hashFunction) {
+        this.hashFn = hashFunction || JSON.stringify;
+        this.values = {};
+        this.size = 0;
+    }
+
+    Set.prototype = {
+        /**
+         * @param value
+         * @returns {boolean} true when item was added
+         */
+        add: function add(value) {
+            var r = !this.contains(value);
+            if (r) {
+                //does not contain
+                this.values[this.hashFn(value)] = value;
+                this.size++;
+            }
+            return r;
+        },
+        /**
+         * @param value
+         * @returns {boolean} true when item was replaced, false when just added
+         */
+        addReplace: function addReplace(value) {
+            var r = this.remove(value);
+            this.add(value);
+            return r;
+        },
+        /**
+         * @param {*} obj
+         * @returns {*} stored value or null if the hash did not found anything
+         */
+        getValue: function getValue(obj) {
+            var hash = this.hashFn(obj);
+            if (this.values.hasOwnProperty(hash)) {
+                return this.values[hash];
+            }
+            return null;
+        },
+        /**
+         * @param value
+         * @returns {boolean} whether it removed the item
+         */
+        remove: function remove(value) {
+            var r = this.contains(value);
+            if (r) {
+                // does copntain
+                delete this.values[this.hashFn(value)];
+                this.size--;
+            }
+            return r;
+        },
+        /**
+         * @param value
+         * @returns {boolean}
+         */
+        contains: function contains(value) {
+            return typeof this.values[this.hashFn(value)] !== "undefined";
+        },
+        /**
+         *
+         * @returns {number}
+         */
+        size: function size() {
+            return this.size;
+        },
+        /**
+         * runs iteratorFunction for each value of set
+         * @param {Function} iteratorFunction
+         * @param {*} [thisObj] this context for iterator
+         */
+        each: function each(iteratorFunction, thisObj) {
+            for (var value in this.values) {
+                var contx = thisObj || this.values[value];
+                iteratorFunction.call(contx, this.values[value]);
+            }
+        },
+        /**
+         *
+         * @returns {Array}
+         */
+        toArray: function () {
+            var r = [];
+            for(var i in this.values){
+                r.push(this.values[i]);
+            }
+            return r;
+        }
+    };
+    return Set;
+});
