54cd2908dbdd3ca842a708a6299e515ce6e0fb05
Jiri Spac <capajj@gmail.com>
2014-04-10 01:25:07 +0200

    just two comments

--- src/trustAsHtml.js
+++ src/trustAsHtml.js
@@ -1,3 +1,4 @@
+// thx to Leeroy Brun http://stackoverflow.com/a/21254635
 angular.module('ngTools').filter('trustAsHtml', ['$sce', function($sce){
 	return function(text) {
 		return $sce.trustAsHtml(text);
