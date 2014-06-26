e8be38bbf65155af1fd2cc491d561264ad2bdcdb
Jiri Spac <jiri.spac.contractor@avg.com>
2014-06-26 18:47:02 +0200

    two translate util directives added

---
+++ src/translate-utils.js
@@ -0,0 +1,49 @@
+directives.directive('longTextClass', function() {
+	return {
+		restrict: 'A',
+		link: function (scope, el, attrs) {
+			scope.$watch(function () {
+				return el.text().trim();
+			}, function (nV) {
+				if (nV.length > attrs.longTextClass) {
+					el.addClass('long_text');
+				} else {
+					el.removeClass('long_text');
+				}
+			});
+		}
+	};
+}).directive('textOverflowClass', function () {
+	function checkOverflow(el, attrs){
+		var curOverflow = el.style.overflow;
+		if ( !curOverflow || curOverflow === "visible" ){
+			el.style.overflow = "hidden";
+		}
+		var isOverflowing;
+		if (!attrs.textOverflowClass) { //use text-overflow-class="true" when you want to check width and height for overflow
+			isOverflowing = el.clientWidth < el.scrollWidth;
+		} else {
+			isOverflowing = el.clientWidth < el.scrollWidth || el.clientHeight < el.scrollHeight;
+		}
+
+		el.style.overflow = curOverflow;
+
+		return isOverflowing;
+	}
+	return {
+		restrict: 'A',
+		link: function (scope, el, attrs) {
+			scope.$watch(function () {
+				return el.text();
+			}, function () {
+
+				if (checkOverflow(el[0], attrs)) {
+					el.addClass('text_overflow');
+				} else {
+					el.removeClass('text_overflow');
+				}
+
+			});
+		}
+	};
+});
